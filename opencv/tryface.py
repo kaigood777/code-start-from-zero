75849265dc79528731b55a106a2d855c4b821adb
kaigood777 <kaigood00@gmail.com>
2025-07-13 15:21:03 +0800

    tryface.py

--- opencv/tryface.py
+++ opencv/tryface.py
@@ -1,29 +1,28 @@
 import cv2
 import numpy as np
-detector = cv2.CascadeClassifier('D:\\learning\\picture opencv\\face_detect.xml')  # 載入人臉追蹤模型
-recog = cv2.face.LBPHFaceRecognizer_create ()      # 啟用訓練人臉模型方法
-faces = []   # 儲存人臉位置大小的串列
-ids = []     # 記錄該人臉 id 的串列
+detector = cv2.CascadeClassifier('D:\\learning\\picture opencv\\face_detect.xml')  
+recog = cv2.face.LBPHFaceRecognizer_create ()     
+faces = [] 
+ids = [] 
 
 for i in range(1,21):
-    img = cv2.imread('D:\\learning\\picture opencv\\use picture\\{i}.jpg')           # 依序開啟每一張蔡英文的照片
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 色彩轉換成黑白
-    img_np = np.array(gray,'uint8')               # 轉換成指定編碼的 numpy 陣列
-    face = detector.detectMultiScale(gray)        # 擷取人臉區域
+    img = cv2.imread('D:\\learning\\picture opencv\\use picture\\{i}.jpg')  
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    img_np = np.array(gray,'uint8')
+    face = detector.detectMultiScale(gray) 
     for(x,y,w,h) in face:
-        faces.append(img_np[y:y+h,x:x+w])         # 記錄蔡英文人臉的位置和大小內像素的數值
-        ids.append(1)                             # 記錄蔡英文人臉對應的 id，只能是整數，都是 1 表示蔡英文的 id 為 1
-
+        faces.append(img_np[y:y+h,x:x+w]) 
+        ids.append(1)      
 for i in range(1,21):
-    img = cv2.imread('D:\\learning\\picture opencv\\han picture\\images({i}).jpg')           # 依序開啟每一張川普的照片
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 色彩轉換成黑白
-    img_np = np.array(gray,'uint8')               # 轉換成指定編碼的 numpy 陣列
-    face = detector.detectMultiScale(gray)        # 擷取人臉區域
+    img = cv2.imread('D:\\learning\\picture opencv\\han picture\\images({i}).jpg') 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    img_np = np.array(gray,'uint8')  
+    face = detector.detectMultiScale(gray)  
     for(x,y,w,h) in face:
-        faces.append(img_np[y:y+h,x:x+w])         # 記錄川普人臉的位置和大小內像素的數值
-        ids.append(2)                             # 記錄川普人臉對應的 id，只能是整數，都是 2 表示川普的 id 為 2
+        faces.append(img_np[y:y+h,x:x+w])  
+        ids.append(2)
 
-print('training...')                              # 提示開始訓練
-recog.train(faces,np.array(ids))                  # 開始訓練
-recog.save('face.yml')                            # 訓練完成儲存為 face.yml
+print('training...')  
+recog.train(faces,np.array(ids)) 
+recog.save('face.yml')    
 print('ok!')
